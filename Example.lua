867be52e061e59e0c5c3c00331c03dae9395348f
j0z4fx <276133711+j0z4fx@users.noreply.github.com>
2026-07-02 16:55:52 -0700

    Pin example to body selector proportions

--- Example.lua
+++ Example.lua
@@ -1,7 +1,7 @@
 -- New example script written by wally
 -- You can suggest changes with a pull request or something
 
-local repo = 'https://raw.githubusercontent.com/j0z4fx/PureUI-v2/bd06f18/'
+local repo = 'https://raw.githubusercontent.com/j0z4fx/PureUI-v2/439fa89/'
 local cacheBust = '?v=' .. tostring(os.time())
 
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua' .. cacheBust))()
