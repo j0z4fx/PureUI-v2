8d890d9ae6817fc8c31f9c6e4263164faa46df07
j0z4fx <276133711+j0z4fx@users.noreply.github.com>
2026-07-01 21:45:31 -0700

    Pin example loader to fixed library commit

--- Example.lua
+++ Example.lua
@@ -1,7 +1,7 @@
 -- New example script written by wally
 -- You can suggest changes with a pull request or something
 
-local repo = 'https://raw.githubusercontent.com/j0z4fx/PureUI-v2/main/'
+local repo = 'https://raw.githubusercontent.com/j0z4fx/PureUI-v2/66671d4/'
 local cacheBust = '?v=' .. tostring(os.time())
 
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua' .. cacheBust))()
